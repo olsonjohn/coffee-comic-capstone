f81f48c15b78298ed0d34da8bef61b367db0e05a
John Olson <n0atn86@gmail.com>
2021-04-06 12:44:39 -0500

    changed add_comment view to class based view

--- config/urls.py
+++ config/urls.py
@@ -5,23 +5,23 @@ The `urlpatterns` list routes URLs to views. For more information please see:
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
 from django.contrib import admin
 from django.urls import path
 
-from ComicBaseApp.views import index, add_comment, signup_view, login_view, logout_view
+from ComicBaseApp.views import index, AddCommentView, signup_view, login_view, logout_view
 
 urlpatterns = [
     path("", index, name="home"),
     path("admin/", admin.site.urls),
-    path("add_comment/", add_comment, name="add_comment"),
+    path("add_comment/", AddCommentView.as_view(), name="add_comment"),
     path("signup/", signup_view, name="signup"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
