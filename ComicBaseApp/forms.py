cbdc2b702941ceba9f7f2c7f96d1a93996439c24
leannjames <gracefulmonk93@yahoo.com>
2021-04-06 12:37:31 -0400

    Added Login and SignUp forms to forms.py

--- ComicBaseApp/forms.py
+++ ComicBaseApp/forms.py
@@ -2,13 +2,17 @@ from django import forms
 from ComicBaseApp.models import ComicUser, ComicBook, ComicComment
 
 
-# Authenication Signup Form
-
-
-
 # Authenication Login Form
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
 
 
+# Authenication Signup Form
+class SignUpForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
+    display_name = forms.CharField(max_length=50)
 
 
 class CommentForm(forms.ModelForm):
