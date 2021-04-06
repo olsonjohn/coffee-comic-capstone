3cfb5246371e8b88140c129ab20e21fd32c66262
John Olson <n0atn86@gmail.com>
2021-04-06 12:30:48 -0500

    Created Index view/url
    added black to dev dependcnecies

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from ComicBaseApp.views import add_comment, signup_view, login_view, logout_view
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('add_comment/', add_comment, name='add_comment'),
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+from ComicBaseApp.views import index, add_comment, signup_view, login_view, logout_view
 
+urlpatterns = [
+    path("", index, name="home"),
+    path("admin/", admin.site.urls),
+    path("add_comment/", add_comment, name="add_comment"),
+    path("signup/", signup_view, name="signup"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
 ]
