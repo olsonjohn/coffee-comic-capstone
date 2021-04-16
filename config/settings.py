64cf2ec688cd4f39177c60bb5572c0abacf624eb
John Olson <n0atn86@gmail.com>
2021-04-15 19:40:55 -0500

    static files

--- config/settings.py
+++ config/settings.py
@@ -126,8 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/hyper/app/static/'
 STATICFILES_DIRS = [
-   BASE_DIR / 'static'
+   BASE_DIR / 'static',
+   BASE_DIR / 'staticfiles'
 ]
 
 LOGIN_URL = '/login/'
