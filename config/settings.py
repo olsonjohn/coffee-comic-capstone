7c21dfe51772075a9b9045845b26b16182e5a7f6
John Olson <n0atn86@gmail.com>
2021-04-17 16:39:17 -0500

    added footer and changed static files dir reference in settings.py

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/hyper/app/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
    BASE_DIR / 'staticfiles'
 ]
